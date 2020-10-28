c3920be3bdb33233540ecd7d9d029cdc0e05cb65
Joshua Hunsche Jones <joshua@hunschejones.com>
2020-10-27 21:03:02 -0700

    Add site top nav

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -5,18 +5,4 @@ export default class extends Controller {
     const flash = e.target.parentNode;
     flash.parentNode.removeChild(flash);
   }
-
-  fadeOut(target) {
-    var fadeEffect = setInterval(function () {
-      if (!target.style.opacity) {
-        target.style.opacity = 1;
-      }
-      if (target.style.opacity > 0) {
-        target.style.opacity -= 0.05;
-      } else {
-        clearInterval(fadeEffect);
-        target.parentNode.removeChild(target);
-      }
-    }, 50);
-  }
 }
---
+++ app/javascript/controllers/main_nav_controller.js
@@ -0,0 +1,38 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "mainNavMenu", "mainNavBurger" ]
+
+  initialize() {
+    document.addEventListener("click", this.closeMenu.bind(this));
+  }
+
+  closeMenu(e) {
+    if (!this.hasClass(e.target, "navbar")) {
+      this.mainNavMenuTarget.classList.remove('is-active');
+      this.mainNavBurgerTarget.classList.remove('is-active');
+    }
+  }
+
+  toggle() {
+    this.mainNavMenuTarget.classList.toggle('is-active');
+    this.mainNavBurgerTarget.classList.toggle('is-active');
+  }
+
+  /**
+   * Searches through the DOM tree to see if a class or it's parent has
+   * a given class.
+   *
+   * @param {element} element The element to start with
+   * @param {element} className The class name to search for
+   */
+  hasClass(element, className) {
+    do {
+      if (element.classList && element.classList.contains(className)) {
+        return true;
+      }
+      element = element.parentNode;
+    } while (element);
+    return false;
+  }
+}
