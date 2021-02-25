043497e80c791344a78d2fbdce50f5258eb7d16a
Joshua Hunsche Jones <joshua@hunschejones.com>
2021-02-25 11:06:31 -0800

    Stop page scroll under loading spinner

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -9,10 +9,13 @@ export default class extends Controller {
       this.searchInputTarget.value.length,
       this.searchInputTarget.value.length
     );
-    this.loaderWrapperTarget.classList.remove("is-active");
+    // this.loaderWrapperTarget.classList.remove("is-active");
   }
 
   loading() {
     this.loaderWrapperTarget.classList.add("is-active");
+    // Stop page from scrolling under spinner
+    document.body.style.overflow = "hidden";
+    document.body.style.height = "100vh";
   }
 }
